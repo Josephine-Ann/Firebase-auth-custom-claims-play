7a049be9f3df162482f421d6e2093d57f0cde992
Josephine-Ann <josephineanngeoghegan@gmail.com>
2020-09-30 13:55:45 +0200

    marketing database

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const app = require("express")()
 const admin = require("firebase-admin");
-const serviceAccount = require("./react-my-mini-blog-firebase-admin-key.json");
+const serviceAccount = require("./service-account-file.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://react-my-mini-blog-2ac12.firebaseio.com"
+    databaseURL: "https://marketing-app-97cfc.firebaseio.com"
 });
+
 
 // TO RUN THIS FILE AND CHECK BASIC CONCEPTS:
 // 1. Navigate to folder in terminal and run node server.js
@@ -13,17 +14,17 @@ admin.initializeApp({
 // ...should say 'done'. This is because of ***
 // 3. Then navigate to localhost:4000/getAdmin and the terminal should say 
 // ... whether the user is an admin or not
-// 4. change the value set in setAdmin again and continue to test the concept
+// 4. Switch the value set in setAdmin again and continue to test the concept
 
 app.get('/setAdmin', async () => {
-    admin.auth().setCustomUserClaims('MXgzzIVO28VaWPvYriyrQy7VmPU2', { admin: false })
+    admin.auth().setCustomUserClaims('A7N5o8JV6RNFifaoiTo2PojcJJa2', { admin: true })
         .then(() => console.log('done'))
     // *** this part here, which hard codes admin: false on custom claims of this
     //user id
 })
 
 app.get('/getAdmin', async () => {
-    admin.auth().getUser('MXgzzIVO28VaWPvYriyrQy7VmPU2').then((userRecord) => {
+    admin.auth().getUser('A7N5o8JV6RNFifaoiTo2PojcJJa2').then((userRecord) => {
         let admin = userRecord.customClaims['admin']
         if (admin) {
             console.log('User is admin')
